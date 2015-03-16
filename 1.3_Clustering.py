007f9ce25bac639d1a4e2fc683823fe5b6ea7f60
Bence Komarniczky <niczky12@gmail.com>
2015-03-16 11:26:22 +0000

    Clustering file ready for live action

---
+++ 1.3_Clustering.py
@@ -0,0 +1,27 @@
+"""
+Description:    Run clustering algorithm on all valid tweets in the dataset.
+                Attach address info and cluster info to all tweets.
+Author:         Bence Komarniczky
+Date:           16/March/2015
+Python version: 3.4
+"""
+
+import ons_twitter.cluster as cl
+from datetime import datetime
+
+# start counting time
+start_time = datetime.now()
+print("Starting clustering: ", start_time, "\n")
+
+# specify databases
+mongo_address = ["192.168.0.82:27017", "twitter", "address"]
+twitter_data = ["192.168.0.97:30000", "twitter", "tweets"]
+
+# start clustering
+if __name__ == "__main__":
+    user_no = cl.cluster_all(twitter_data, mongo_address)
+
+    # give info
+    print("\n  ****\nFinished clustering at: ", datetime.now(),
+          "\n in: ", datetime.now()-start_time,
+          "\n Found users:", user_no)
