299926c3d1b3588d76d24b518a6c4597a5a1c87c
Bence Komarniczky <niczky12@gmail.com>
2015-03-17 10:48:25 +0000

    prepare for demo

--- Test_file.py
+++ Test_file.py
@@ -10,35 +10,13 @@ from ons_twitter.data_import import *
 from ons_twitter.data_formats import *
 
 
-mongo_address = ("192.168.0.82:27017", "twitter", "address")
+mongo_address = ("127.0.0.1:27017", "twitter", "address")
 original_file = "C:/Users/ONS-BIG-DATA/Documents/TWITTER/twitter/data/input/Tweets_Apr_Oct.csv"
-#create_partition_csv(original_file, "data/input/chunks/",  num_rows=-1, chunk_size=10000)
+create_partition_csv(original_file, "data/input/chunk_test/",  num_rows=10000, chunk_size=1000)
 
-test_file = "C:/Users/ONS-BIG-DATA/Documents/TWITTER/twitter/data/input/Tweets_Apr_Oct_test_subset0.csv"
-#
-# test_twitter_mongo = ("192.168.0.97:30000", "twitter", "tweets")
-#
-# if __name__ == "__main__":
-#     a = import_csv("data/input/chunk_test",
-#                mongo_connection=test_twitter_mongo,
-#                mongo_address=mongo_address)
+test_twitter_mongo = ("127.0.0.1:27017", "test", "tweets")
 
-
-
-# get_diagnostics = import_one_csv(original_file,
-#                                  mongo_connection=test_twitter_mongo,
-#                                  mongo_address=mongo_address,
-#                                  debug=False,
-#                                  header=False,
-#                                  debug_rows=1000,
-#                                  print_progress=1000)
-#
-# # 1.06 minutes for 20,000 entries using csv reader
-# # 34s for 10,000 entries using csv reader
-#
-# print(get_diagnostics)
-
-# address_base_loc = "data/input/address/address_base.csv"
-# address_base = AddressBase("data/output/address/", 100000)
-# a = address_base.import_address_csv(address_base_loc, terminate_at=-1)
-# print(a)
+if __name__ == "__main__":
+    a = import_csv("data/input/chunk_test",
+                   mongo_connection=test_twitter_mongo,
+                   mongo_address=mongo_address)
--- Test_file2.py
+++ Test_file2.py
@@ -9,62 +9,13 @@ from datetime import datetime
 import ons_twitter.cluster as cl
 
 start_time = datetime.now()
-import pymongo
 
-address_mongo = pymongo.MongoClient("192.168.0.82:27017").twitter.address
+address_mongo = ("127.0.0.1:27017", "twitter", "address")
 
-from ons_twitter.data_formats import *
+test_twitter_mongo = ("127.0.0.1:27017", "test", "tweets")
 
-test_twitter_mongo = pymongo.MongoClient("127.0.0.1:27017").test.tweets
-
-tweets_by_user = cl.create_dictionary_for_chunk(test_twitter_mongo, 3)
-
-print(tweets_by_user)
-
-for key in tweets_by_user.keys():
-    print(tweets_by_user[key])
-    print("\n\n New user!!!")
-    cl.cluster_one_user(key, tweets_by_user, test_twitter_mongo, address_mongo)
-
-#
-#
-# trial = [['A', 908190000, [373820, 805600]],
-#          ['B', 908190000, [373823, 805679]],
-#          ['C', 908190000, [373825, 805684]],
-#          ['D', 908190000, [373838, 805600]],
-#          ['E', 908190000, [373838, 805600]],
-#          ['F', 908190000, [12, 805600]],
-#          ['G', 908190000, [8, 805600]]
-#          ]
-# print(datetime.now() - start_time)
-# trial *= 100
-#
-# import numpy as np
-#
-#
-# distance_array = cl.distance_matrix(trial, block_size=10)
-#
-#
-# remaining_mask = [[x for x in range(len(trial))],
-#                   np.array([x for x in range(len(trial))], dtype="int32")]
-# print(datetime.now() - start_time)
-#
-# continue_clustering = True
-#
-# all_clusters = []
-# while continue_clustering:
-#     new_cluster, remaining_mask = cl.create_one_cluster(trial, remaining_mask, distance_array)
-#     if new_cluster is not None:
-#         all_clusters.append(new_cluster)
-#     else:
-#         continue_clustering = False
-#
-# index = 0
-# for cluster in all_clusters:
-#     index += 1
-#     a = cl.create_cluster_info(cluster, index, address_mongo)
-#     print(a)
-#     print(cluster)
-
+if __name__ == "__main__":
+    cl.cluster_all(mongo_connection=test_twitter_mongo,
+                   mongo_address=address_mongo)
 
 print(datetime.now() - start_time)
