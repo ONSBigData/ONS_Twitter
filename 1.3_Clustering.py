bf6279b0c19b3acb53bd0ca1076b274c12fb0544
Bence Komarniczky <niczky12@gmail.com>
2015-03-27 10:38:00 +0000

    do it in serial

--- 1.3_Clustering.py
+++ 1.3_Clustering.py
@@ -17,19 +17,13 @@ print("Starting clustering: ", start_time, "\n")
 
 # specify databases
 
-mongo_address = (("192.168.0.82:27017", "twitter", "address"),
-                 ("192.168.0.87:28000", "twitter", "address"),
-                 ("192.168.0.62:28001", "twitter", "address"),
-                 ("192.168.0.97:28002", "twitter", "address"),
-                 ("192.168.0.97:28003", "twitter", "address"))
+mongo_address = ("192.168.0.82:27017", "twitter", "address")
 
-twitter_data = (("localhost:30033", "twitter", "tweets"),
-                ("192.168.0.97:30000", "twitter", "tweets"),
-                ("192.168.0.97:30030", "twitter", "tweets"))
+twitter_data = ("localhost:30033", "twitter", "tweets")
 
 # start clustering
 user_no = cl.cluster_all(twitter_data, mongo_address, chunk_range=range(12, 1000),
-                         parallel=True, debug=False)
+                         parallel=False, debug=False)
 
 # give info
 print("\n  ****\nFinished clustering at: ", datetime.now(),
