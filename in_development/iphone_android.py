5f36bd77ff66f02a719485e64682a982b20a1037
Bence Komarniczky <niczky12@gmail.com>
2015-04-29 10:01:06 +0000

    do it in parallel

--- in_development/iphone_android.py
+++ in_development/iphone_android.py
@@ -9,6 +9,8 @@ import os
 import gzip
 from datetime import date, timedelta, datetime
 
+from joblib import Parallel, delayed
+
 import pandas as pd
 
 
@@ -16,21 +18,31 @@ folder = "/nas/data/Twitter Data/newdata/Complete Aug_Oct/"
 
 
 def read_tweets(file_path):
+    print(file_path, datetime.now())
+
     infile = gzip.open(file_path, "r")
     file_content = infile.readlines()
-    local_tweets = []
 
+    counter = 0
     for line in file_content:
         try:
             new_tweet = json.loads(line.decode("utf-8"))
             if len(new_tweet) > 1:
-                local_tweets.append(new_tweet)
+                if counter == 0:
+                    this_date = datetime.strptime(
+                        new_tweet["created_at"], "%a %b %d %H:%M:%S %z %Y").date().strftime("%Y,%m,%d")
+                    this_date = this_date.split(",")
+                    numeric_date = [int(dummy) for dummy in this_date]
+                    pandas_table = create_table(numeric_date, numeric_date)
+
+                parse_tweet(new_tweet, pandas_table)
         except ValueError:
             # print("Conversion failed!", line)
             continue
+        counter += 1
 
     infile.close()
-    return local_tweets
+    return pandas_table
 
 
 def create_table(start, end):
@@ -50,35 +62,35 @@ def create_table(start, end):
     return returned_table
 
 
-def count_sources(tweet_list, data_table):
-    for tweet in tweet_list:
-        this_date = datetime.strptime(tweet["created_at"], "%a %b %d %H:%M:%S %z %Y").date()
-        source = tweet["source"]
+def parse_tweet(tweet, data_table):
+    this_date = datetime.strptime(tweet["created_at"], "%a %b %d %H:%M:%S %z %Y").date()
+    source = tweet["source"]
 
-        found_something = False
-        data_table["total"][this_date] += 1
+    found_something = False
+    data_table["total"][this_date] += 1
 
-        for search_this in data_table.columns.values[:-1]:
-            if source.find(search_this) > 0:
-                found_something = True
-                data_table[search_this][this_date] += 1
-                break
+    for search_this in data_table.columns.values[:-2]:
+        if source.find(search_this) > 0:
+            found_something = True
+            data_table[search_this][this_date] += 1
+            break
 
-        if not found_something:
-            data_table["other"][this_date] += 1
-
-    return data_table
+    if not found_something:
+        data_table["other"][this_date] += 1
 
 
-my_table = create_table((2014, 8, 15), (2014, 10, 31))
+#
+# for file in os.listdir(folder)[0:1]:
+# filename = folder + file
+#     my_table = read_tweets(filename, my_table)
 
-counter = 0
-for file in os.listdir(folder):
-    counter += 1
-    filename = folder + file
-    if counter % 100 == 1:
-        print(counter, filename, datetime.now())
-    tweets = read_tweets(filename)
-    my_table = count_sources(tweets, my_table)
+
+file_names = [(folder + fn) for fn in os.listdir(folder)]
+
+tables = Parallel(n_jobs=8)(delayed(read_tweets)(file_path) for file_path in file_names)
+
+my_table = tables[0]
+for table in tables[1:]:
+    my_table = my_table.add(table, fill_value=0)
 
 my_table.to_csv("sources.csv")
