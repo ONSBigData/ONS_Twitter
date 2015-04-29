6afbfcef0835efa48418322c73096ab71f3ac7f8
Bence Komarniczky <niczky12@gmail.com>
2015-04-29 08:59:59 +0100

    read in compressed files

--- in_development/iphone_android.py
+++ in_development/iphone_android.py
@@ -6,47 +6,69 @@ Date: 28/04/2015
 
 import json
 import os
+import gzip
 
 
-all_percents = []
-all_tweets = 0
+filename = "data/" + os.listdir("data/")[0]
 
-apple_users = {"iOS": 0, "iPad": 0, "iPhone": 0}
-for fn in os.listdir("data/oct/"):
-    tweets = []
+infile = gzip.open(filename, "r")
+file_content = infile.readlines()
+tweets = []
 
-    filename = "data/oct/" + fn
-    with open(filename, 'r') as infile:
-        for row in infile:
-            try:
-                len(row)
-                new_tweet = json.loads(row)
-                if len(new_tweet.keys()) > 2:
-                    tweets.append(new_tweet)
-            except ValueError:
-                continue
+for line in file_content:
+    try:
+        new_tweet = json.loads(line.decode("utf-8"))
+        if len(new_tweet) > 1:
+            tweets.append(new_tweet)
+    except ValueError:
+        # print("Conversion failed!", line)
+        continue
 
-    sources = []
+infile.close()
 
-    for one_tweet in tweets:
-        sources.append(one_tweet["source"])
+for tweet in tweets[-5:-1]:
+    print(tweet)
 
-    all_tweets += len(tweets)
-    search_these = ("iOS", "iPad", "iPhone")
-    apple_set = set([])
-
-    for source in sources:
-        source = str(source)
-        for query in search_these:
-            if source.find(query) > -1:
-                apple_users[query] += 1
-                apple_set.add(source)
-            else:
-                continue
-
-    print(apple_users)
-    all_percents.append(apple_users["iPhone"] / len(tweets))
-
-print((apple_users["iPhone"] + apple_users["iOS"] + apple_users["iPad"]) / all_tweets)
-
-print((apple_users["iPhone"]) / all_tweets)
+#
+# all_percents = []
+# all_tweets = 0
+#
+# apple_users = {"iOS": 0, "iPad": 0, "iPhone": 0}
+# for fn in os.listdir("data/oct/"):
+#     tweets = []
+#
+#     filename = "data/oct/" + fn
+#     with open(filename, 'r') as infile:
+#         for row in infile:
+#             try:
+#                 len(row)
+#                 new_tweet = json.loads(row)
+#                 if len(new_tweet.keys()) > 2:
+#                     tweets.append(new_tweet)
+#             except ValueError:
+#                 continue
+#
+#     sources = []
+#
+#     for one_tweet in tweets:
+#         sources.append(one_tweet["source"])
+#
+#     all_tweets += len(tweets)
+#     search_these = ("iOS", "iPad", "iPhone")
+#     apple_set = set([])
+#
+#     for source in sources:
+#         source = str(source)
+#         for query in search_these:
+#             if source.find(query) > -1:
+#                 apple_users[query] += 1
+#                 apple_set.add(source)
+#             else:
+#                 continue
+#
+#     print(apple_users)
+#     all_percents.append(apple_users["iPhone"] / len(tweets))
+#
+# print((apple_users["iPhone"] + apple_users["iOS"] + apple_users["iPad"]) / all_tweets)
+#
+# print((apple_users["iPhone"]) / all_tweets)
